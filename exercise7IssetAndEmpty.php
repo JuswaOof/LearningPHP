b91936c85e36980fc746d361bfe43903a94984f3
Joshua Salcedo <71166003+JuswaOof@users.noreply.github.com>
2023-09-18 21:51:54 +0800

    Create exercise7IssetAndEmpty.php

---
+++ exercise7IssetAndEmpty.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Document</title>
+</head>
+<body>
+ <form action="issetAndEmpty.php" method="POST">
+  <label for="">username:</label>
+  <input type="text" name="username">
+  <br>
+  <br>
+  <label for="">password:</label>
+  <input type="text" name="password">
+  <br><br>
+  <input type="submit" name="login" value="login">
+
+ </form>
+</body>
+</html>
+
+<?php
+
+// foreach($_POST as $key => $value){
+//  echo "{$key} = {$value} <br>";
+// }
+
+if(isset($_POST["login"])){
+
+ $username = $_POST["username"];
+ $password = $_POST["password"];
+
+ if(empty($username) && empty($password)){
+  echo "please enter username and password";
+ }
+ else{
+ if(empty($username)){
+  echo "please input username";
+ }elseif(empty($password)){
+  echo "please input password";
+ }
+ else{
+  echo "hello {$username}";
+ }
+}
+}
+
+?>
