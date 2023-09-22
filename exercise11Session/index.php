5db5c98adb062cb70e2388a5fa2f4b2e8347fce5
Joshua Salcedo <71166003+JuswaOof@users.noreply.github.com>
2023-09-22 21:21:03 +0800

    Update index.php

--- exercise11Session/index.php
+++ exercise11Session/index.php
@@ -1,1 +1,44 @@
 
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Document</title>
+</head>
+<body>
+ <h1>Session</h1>
+<hr>
+ <h2>This is the login page</h2>
+
+ 
+<form action="index.php" method="POST">
+<label for="">username:</label>
+<input type="text" name="username"><br><br>
+<label for="">password:</label>
+<input type="password" name="password"><br><br>
+<input type="submit" name="submit" value="submit">
+</form>
+<hr>
+
+</body>
+</html>
+
+<?php
+
+if(isset($_POST["submit"])){
+ 
+ if(!empty($_POST["password"]) && !empty($_POST["username"])){
+ $_SESSION["username"] = $_POST["username"];
+ $_SESSION["password"] = $_POST["password"];
+
+header("Location: home.php");
+
+ }else{
+  echo "Please input username and password";
+ }
+}
+?>
