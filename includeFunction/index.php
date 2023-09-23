98d10ed6c125ab2162502f4917612b9da9e49610
Joshua Salcedo <71166003+JuswaOof@users.noreply.github.com>
2023-09-23 22:42:41 +0800

    Update index.php

--- includeFunction/index.php
+++ includeFunction/index.php
@@ -1,1 +1,19 @@
+<?php 
+include("header.html");
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Document</title>
+</head>
+<body>
+ <h2>This is the homepage</h2>
+</body>
+</html>
+
+<?php 
+include("footer.html");
+?>
