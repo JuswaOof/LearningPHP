1d1d3f731960be4b96fdfb6bdf04ec80ce63e625
Joshua Salcedo <71166003+JuswaOof@users.noreply.github.com>
2023-09-25 20:25:46 +0800

    Update exercise13Cookies.php

--- exercise13Cookies.php
+++ exercise13Cookies.php
@@ -1,1 +1,14 @@
 
+<?php 
+ setcookie("Key", "value", time() + (86400 * 2), "/");
+ setcookie("FavFood", "Spaghetti", time() + (86400 * 2), "/");
+ setcookie("FavGame", "Dota", time() + (86400 * 2), "/");
+
+ 
+
+ if(isset($_COOKIE["FavFood"])){
+  echo "Your favorite food is {$_COOKIE["FavFood"]}";
+ }else{
+  echo "You don't have favorite food";
+ }
+?>
