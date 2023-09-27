b53ae44aa96648e9ce4f00707aa5fa020f935cbd
Joshua Salcedo <71166003+JuswaOof@users.noreply.github.com>
2023-09-27 21:42:56 +0800

    Update conn.php

--- exercise15/conn.php
+++ exercise15/conn.php
@@ -1,1 +1,17 @@
 
+<?php 
+ $db_server = "localhost";
+ $db_username = "root";
+ $db_password = "";
+ $db_name = "testdb";
+
+ $conn = "";
+
+ try {
+    $conn = mysqli_connect($db_server, $db_username, $db_password, $db_name);
+} catch (Exception $e) {
+    // Handle the exception here, for example, by logging it or displaying an error message.
+    echo 'Error: ' . $e->getMessage();
+}
+
+?>
